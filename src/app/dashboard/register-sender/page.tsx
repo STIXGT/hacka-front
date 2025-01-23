7255b36c05ca1346e9dcfbc5db6839d506056301
STIXGT <stiveguano@gmail.com>
2025-01-23 14:06:23 -0500

    Add sender register view

---
+++ src/app/dashboard/register-sender/page.tsx
@@ -0,0 +1,9 @@
+import ProfileForm from "@/components/ProfileForm";
+
+export default function RegisterProvider() {
+  return (
+    <section className="">
+      <ProfileForm />
+    </section>
+  );
+}
